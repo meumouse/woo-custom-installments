d6f547ba31c74ef4dc209fd8d5353d611d5b6383
MeuMouse.com <95049087+meumouse@users.noreply.github.com>
2024-08-16 13:08:36 -0300

    Add files via upload

--- assets/front/js/woo-custom-installments-range-price.js
+++ assets/front/js/woo-custom-installments-range-price.js
@@ -2,83 +2,91 @@
  * Replace range price
  * 
  * @since 2.8.0
- * @version 4.5.0
+ * @version 4.5.1
+ * @package MeuMouse.com
  */
 jQuery(document).ready( function($) {
     var product_price_container = $('#woo-custom-installments-product-price');
     var siblings_price = product_price_container.siblings('.woo-custom-installments-group.variable-range-price');
     var original_price = get_original_price();
 
-    /**
-     * Get original price
-     * 
-     * @since 2.8.0
-     * @version 4.5.0
-     * @returns string
-     */
     function get_original_price() {
-        var container_price = product_price_container.closest('.woo-custom-installments-group');
-        container_price.closest('div').addClass('range-price');
-
-        return container_price.html();
+        return product_price_container.html();
     }
 
     // on load page
     update_price_html(original_price);
 
-    /**
-     * When a variation is found
-     */
+    // when a variation is finded
     $(document).on('found_variation', 'form.variations_form', function(e, variation) {
-        update_price_html(variation.price_html);
+        if (variation.variation_id) {
+            update_price_via_ajax(variation.variation_id);
+        }
     });
 
-    /**
-     * When a variation is selected
-     */
+    // when a variation is selected
     $('form.variations_form').on('show_variation', function(e, variation) {
-        update_price_html(variation.price_html);
+        if (variation.variation_id) {
+            update_price_via_ajax(variation.variation_id);
+        }
     });
 
-    /**
-     * On change variation
-     */
     $('form.variations_form').on('change', 'select', function() {
         if ($(this).val() === '') {
             update_price_html(original_price);
         }
     });
 
-    /**
-     * On reset variations
-     */
     $('a.reset_variations').click( function(e) {
         e.preventDefault();
-
-        update_price_html(original_price);
+        
+        product_price_container.addClass('d-none');
+        siblings_price.removeClass('d-none');
     });
 
     /**
-     * Update product price for selected variation
+     * Get price HTML in AJAX
+     * 
+     * @since 4.5.1
+     * @param {int} variation_id | Product variation ID
+     */
+    function update_price_via_ajax(variation_id) {
+        product_price_container.addClass('wci-loading-price');
+        siblings_price.addClass('wci-loading-price');
+
+        $.ajax({
+            url: wci_range_params.ajax_url,
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                action: 'get_updated_price_html',
+                product_id: variation_id,
+            },
+            success: function(response) {
+                if (response.success) {
+                    update_price_html(response.data.price_html);
+                    product_price_container.removeClass('wci-loading-price');
+                    siblings_price.removeClass('wci-loading-price');
+                    siblings_price.addClass('d-none');
+                }
+            },
+            error: function() {
+                update_price_html(original_price);
+                product_price_container.removeClass('wci-loading-price');
+                siblings_price.removeClass('wci-loading-price');
+            }
+        });
+    }
+
+    /**
+     * Update variation price with selected variation 
      * 
      * @since 2.8.0
-     * @version 4.5.0
-     * @param {string} price_html | Product price
+     * @version 4.5.1
+     * @param {string} price_html | Product price HTML
      */
     function update_price_html(price_html) {
-        // Remove any existing nested .price elements
-        product_price_container.find('.price').children().unwrap();
-        
-        if ( ! product_price_container.find('.woo-custom-installments-group').length ) {
-            product_price_container.html(price_html);
-        } else {
-            product_price_container.find('.woo-custom-installments-group').replaceWith(price_html);
-        }
-
-        if ( product_price_container.find('.woo-custom-installments-group').length ) {
-            product_price_container.addClass('active');
-        } else {
-            product_price_container.html(siblings_price);
-        }
+        product_price_container.html('');
+        product_price_container.html(price_html).removeClass('d-none').addClass('active');
     }
 });
