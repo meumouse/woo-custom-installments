9cee92d2825bd95fad82320453c6e1387aa9ac3f
MeuMouse.com <95049087+meumouse@users.noreply.github.com>
2024-09-12 09:08:21 -0300

    Add files via upload

--- assets/front/js/woo-custom-installments-range-price.js
+++ assets/front/js/woo-custom-installments-range-price.js
@@ -2,66 +2,81 @@
  * Replace range price
  * 
  * @since 2.8.0
- * @version 5.0.0
+ * @version 5.1.2
  * @package MeuMouse.com
  */
 jQuery(document).ready( function($) {
     var product_price_container = $('#woo-custom-installments-product-price');
     var siblings_price = product_price_container.siblings('.woo-custom-installments-group.variable-range-price');
     var original_price = get_original_price();
+    var get_quantity = $('input[name="quantity"]').val();
 
     /**
      * Get original price container
      * 
      * @since 2.8.0
-     * @version 5.0.0
+     * @version 5.1.0
      * @returns string
      */
     function get_original_price() {
         var container_price = product_price_container;
 
-        if ( product_price_container.html().trim() === '' ) {
+        if ( product_price_container && product_price_container.html() && product_price_container.html().trim() === '' ) {
             container_price = siblings_price;
         }
 
         return container_price.html();
     }
 
-    // update price on load page
-    update_price_html(original_price);
-
-    // Variation events found or selected
-    $('form.variations_form').on('found_variation show_variation', function (e, variation) {
+    /**
+     * On found or show product variation event
+     * 
+     * @since 2.8.0
+     * @version 5.1.2
+     * @param {object} e | Event type
+     * @param {object} variation | Product variation object
+     */
+    $(document.body).on('found_variation show_variation', function(e, variation) {
         if (variation && variation.variation_id) {
             if (wci_range_params.update_method === 'ajax') {
-                update_price_via_ajax(variation.variation_id);
+                update_price_via_ajax(variation.display_price, get_quantity, variation.variation_id);
             } else {
                 update_price_html(variation.price_html);
             }
         }
     });
 
-    // When changing the variation, if no value is selected, restore the original price
-    $('form.variations_form').on('change', 'select', function () {
-        if ($(this).val() === '') {
-            update_price_html(original_price);
-        }
+    // clear variations
+    $(document.body).on('click', 'a.reset_variation, a.reset_variations', function(e) {
+        e.preventDefault();
+
+        product_price_container.removeClass('active').addClass('d-none').html('');
+        siblings_price.html(original_price).removeClass('d-none');
     });
 
-    // clear variations
-    $('a.reset_variations').click( function(e) {
-        e.preventDefault();
-        product_price_container.removeClass('active').html(original_price);
-        siblings_price.removeClass('d-none');
+    // on change quantity
+    $(document.body).on('change', 'input[name="quantity"]', function() {
+        $('form.variations_form').on('found_variation show_variation', function(e, variation) {
+            if (variation && variation.variation_id) {
+                if (wci_range_params.update_method === 'ajax') {
+                    update_price_via_ajax(variation.display_price, get_quantity, variation.variation_id);
+                }
+            }
+        });
+
+        prevent_duplicate_prices();
     });
 
     /**
      * Get price HTML in AJAX
      * 
      * @since 4.5.1
-     * @param {int} variation_id | Product variation ID
+     * @version 5.1.0
+     * @param {float} price | Product price
+     * @param {int} quantity | Product quantity
+     * @param {int} product_id | Product ID or variation ID
      */
-    function update_price_via_ajax(variation_id) {
+    function update_price_via_ajax(price, quantity, product_id) {
         product_price_container.addClass('wci-loading-price');
         siblings_price.addClass('wci-loading-price');
 
@@ -71,26 +86,48 @@ jQuery(document).ready( function($) {
             dataType: 'json',
             data: {
                 action: 'get_updated_price_html',
-                product_id: variation_id,
+                price: price,
+                quantity: quantity,
+                product_id: product_id,
             },
-            success: function (response) {
+            success: function(response) {
                 if (response.success) {
-                    update_price_html(response.data.price_html);
-                    siblings_price.addClass('d-none');
+                    update_price_html_ajax(response.data.price_html);
                 } else {
-                    update_price_html(original_price);
+                    update_price_html_ajax(original_price);
                 }
 
                 product_price_container.removeClass('wci-loading-price');
                 siblings_price.removeClass('wci-loading-price');
             },
-            error: function () {
-                update_price_html(original_price);
+            error: function() {
+                update_price_html_ajax(original_price);
                 product_price_container.removeClass('wci-loading-price');
                 siblings_price.removeClass('wci-loading-price');
+                product_price_container.removeClass('active').html('');
+
+                prevent_duplicate_prices();
             }
         });
     }
+
+    // get triggers list from backend
+    var triggers = wci_range_params.element_triggers.split(',');
+
+    // Iterate over each trigger and associate a click event
+    triggers.forEach( function(trigger) {
+        $(trigger.trim()).on('click touchstart', function() {
+            $('form.variations_form').on('found_variation show_variation', function(e, variation) {
+                if (variation && variation.variation_id) {
+                    if (wci_range_params.update_method === 'ajax') {
+                        update_price_via_ajax(variation.display_price, get_quantity, variation.variation_id);
+                    } else {
+                        update_price_html(variation.price_html);
+                    }
+                }
+            });
+        });
+    });
 
     /**
      * Update variation price with selected variation 
@@ -106,24 +143,33 @@ jQuery(document).ready( function($) {
 
         product_price_container.html(price_html).removeClass('d-none').addClass('active');
 
-        if ( product_price_container.hasClass('active') && product_price_container.html().trim() !== '' ) {
+        prevent_duplicate_prices();
+    }
+
+    /**
+     * Update variation price with selected variation on AJAX response
+     * 
+     * @since 2.8.0
+     * @version 5.1.0
+     * @param {string} price_html | Product price HTML
+     */
+    function update_price_html_ajax(price_html) {
+        product_price_container.html('');
+        product_price_container.html(price_html).removeClass('d-none').addClass('active');
+        prevent_duplicate_prices();
+    }
+    
+    /**
+     * Check if main price container has content and prevent duplicate info
+     * 
+     * @since 5.1.0
+     */
+    function prevent_duplicate_prices() {
+        if (product_price_container && product_price_container.hasClass('active') && product_price_container.html().trim() !== '' ) {
             siblings_price.addClass('d-none');
+        } else {
+            product_price_container.removeClass('active');
+            siblings_price.removeClass('d-none');
         }
     }
-
-    // Check for duplicates and prevent multiple displays
-    var wci_siblings = $('.range-price .price').siblings('.woo-custom-installments-group');
-
-    if (wci_siblings.length > 0) {
-        wci_siblings.addClass('d-none');
-    }
-
-    var triggers = wci_range_params.element_triggers.split(',');
-
-    // Iterate over each trigger and associate a click event
-    triggers.forEach( function(trigger) {
-        $(trigger.trim()).on('click', function () {
-            update_price_html(original_price);
-        });
-    });
 });
--- assets/front/js/woo-custom-installments-update-table-installments.js
+++ assets/front/js/woo-custom-installments-update-table-installments.js
@@ -2,96 +2,172 @@
  * Update table installments
  * 
  * @since 2.3.5
- * @version 4.5.0
+ * @version 5.1.2
  */
 jQuery(document).ready( function($) {
-
 	/**
 	 * Init object
 	 */
 	var Woo_Custom_Installments = {
   
-	  /**
-	   * Initialize actions
-	   */
-	  init: function() {
-		// Initial load.
-		$(document.body).on('show_variation', function( e, variation, purchasable ) {
-		  Woo_Custom_Installments.update_table( e, variation, purchasable );
-		});
-	  },
-  
-  
-	  /**
-	   * Update table installments
-	   *
-	   * @param {string} field Target
-	   * @param {boolean} copy
-	   */
-	  update_table: function( e, variation, purchasable ) {
-		var tbody = $('.woo-custom-installments-table').find('tbody');
-		tbody.html( '<tr style="display: none !important;"></tr>' );
-  
-		var i = 1;
-		var fees = wci_update_table_params.fees;
+		/**
+		 * Initialize actions
+		 */
+		init: function() {
+			/**
+			 * Initialize action on show or found variation
+			 * 
+			 * @since 2.3.5
+			 * @param {object} e | Event type
+			 * @param {object} variation | Variation product object
+			 */
+			$(document.body).on('show_variation found_variation', function(e, variation) {
+				Woo_Custom_Installments.update_table(e, variation, false);
+			});
 
-		while ( i <= wci_update_table_params.max_installments ) {
-		  var fee = fees.hasOwnProperty(i) ? fees[i] : wci_update_table_params.fee;
+			/**
+			 * Compat with Tiered Price Table plugin
+			 * 
+			 * @since 5.1.0
+			 * @param {object} e | Event type
+			 * @param {object} variation | Variation product object
+			 */
+			$(document).on('tiered_price_update', function(e, variation) {
+				Woo_Custom_Installments.update_table(e, variation.price, true);
+			});
+		},
   
-		  if ( i <= wci_update_table_params.max_installments_no_fee ) {
-			var price = variation.display_price / i;
-  
-			if ( price < wci_update_table_params.min_installment ) {
-			  break;
-			}
-  
-			tbody.append( '<tr class="fee-included"><th>' + tbody.data('default-text').replace( '{{ parcelas }}', i ).replace( '{{ valor }}', Woo_Custom_Installments.get_formatted_price( price ) ).replace( '{{ juros }}', wci_update_table_params.without_fee_label ) + '</th><th>' + Woo_Custom_Installments.get_formatted_price( variation.display_price ) + '</th></tr>' );
-		  } else {
-			if ( wci_update_table_params.fee !== fee ) {
-				// custom fees
-				var fee = fee.toString().replace( ',', '.' ) / 100;
-				var final_cost = variation.display_price + ( variation.display_price * fee );
-				var price = final_cost / i;
-			} else {
-			  	var fee = fee.toString().replace( ',', '.' ) / 100;
-				var exp = Math.pow( 1 + fee, i );
-				var price = variation.display_price * fee * exp / ( exp - 1 );
-				var final_cost = price * i;
-			}
-  
-			if ( price < wci_update_table_params.min_installment ) {
-			  break;
-			}
-  
-			tbody.append( '<tr class="fee-included"><th>' + tbody.data( 'default-text' ).replace( '{{ parcelas }}', i ).replace( '{{ valor }}', Woo_Custom_Installments.get_formatted_price( price ) ).replace( '{{ juros }}', wci_update_table_params.with_fee_label ) + '</th><th>' + Woo_Custom_Installments.get_formatted_price( final_cost ) + '</th></tr>' );
-		  }
-  
-		  i++;
-		}
+		/**
+		 * Update table installments
+		 *
+		 * @since 2.3.5
+		 * @version 5.1.0
+		 * @param {object} e | Event type
+		 * @param {object} variation | Object variation
+		 * @param {boolean} direct_price | If object product is unavailable, send direct price on variation param (Compatibility with Tiered Price Table)
+		 */
+		update_table: function(e, variation, direct_price = false) {
+			var get_price = variation.display_price || variation;
+			var get_variation_id = variation.variation_id || variation;
+			var tbody = $('.woo-custom-installments-table').find('tbody');
+			var default_text = tbody.data('default-text');
+			tbody.html('<tr style="display: none !important;"></tr>');
+		
+			var i = 1;
+			var fees = wci_update_table_params.fees;
+		
+			var last_no_fee_installment = null;
+			var last_fee_installment = null;
+		
+			while (i <= wci_update_table_params.max_installments) {
+				var fee = fees.hasOwnProperty(i) ? fees[i] : wci_update_table_params.fee;
+		
+				if (i <= wci_update_table_params.max_installments_no_fee) {
+					var price = get_price / i;
+		
+					if (price < wci_update_table_params.min_installment) {
+						break;
+					}
+		
+					// Append row without fee (no interest)
+					if (default_text) {
+						tbody.append('<tr class="no-fee"><th>' + default_text.replace('{{ parcelas }}', i).replace('{{ valor }}', Woo_Custom_Installments.get_formatted_price(price)).replace('{{ juros }}', wci_update_table_params.without_fee_label) + '</th><th>' + Woo_Custom_Installments.get_formatted_price(get_price) + '</th></tr>');
+					}
 
-		// AJAX call to get update prices
-		$.ajax({
-			url: wci_update_table_params.ajax_url,
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				action: 'get_updated_variation_prices_action',
-				variation_id: variation.variation_id,
-			},
-			success: function(response) {
-				if ( response.success ) {
-					$.each(response.data, function(key, value) {
-						$(value.element).find('.amount').html(value.price);
-					});
+					// Store the last "no-fee" installment
+					last_no_fee_installment = {
+						installments: i,
+						price: Woo_Custom_Installments.get_formatted_price(price)
+					};
+		
+				} else {
+					if (wci_update_table_params.fee !== fee) {
+						// custom fees
+						var fee = fee.toString().replace(',', '.') / 100;
+						var final_cost = get_price + (get_price * fee);
+						var price = final_cost / i;
+					} else {
+						var fee = fee.toString().replace(',', '.') / 100;
+						var exp = Math.pow(1 + fee, i);
+						var price = get_price * fee * exp / (exp - 1);
+						var final_cost = price * i;
+					}
+		
+					if (price < wci_update_table_params.min_installment) {
+						break;
+					}
+		
+					// Append row with fee (with interest)
+					if (default_text) {
+						tbody.append('<tr class="fee-included"><th>' + default_text.replace('{{ parcelas }}', i).replace('{{ valor }}', Woo_Custom_Installments.get_formatted_price(price)).replace('{{ juros }}', wci_update_table_params.with_fee_label) + '</th><th>' + Woo_Custom_Installments.get_formatted_price(final_cost) + '</th></tr>');
+					}
+
+					// Store the last "fee-included" installment
+					last_fee_installment = {
+						installments: i,
+						price: Woo_Custom_Installments.get_formatted_price(price)
+					};
+				}
+		
+				i++;
+			}
+		
+			// Update main container price elements without altering the surrounding text
+			if ( last_no_fee_installment && wci_update_table_params.check_tiered_plugin === '1' ) {
+				$('.woo-custom-installments-group.variable-range-price').find('.woo-custom-installments-details-without-fee .best-value.no-fee .amount').html(last_no_fee_installment.price);
+				$('.woocommerce-variation-price').find('.woo-custom-installments-details-without-fee .best-value.no-fee .amount').html(last_no_fee_installment.price);
+
+				if ( $('#woo-custom-installments-product-price').hasClass('active') ) {
+					$('#woo-custom-installments-product-price').find('.woo-custom-installments-details-without-fee .best-value.no-fee .amount').html(last_no_fee_installment.price);
 				}
 			}
-		});
-	  },
+		
+			if ( last_fee_installment && wci_update_table_params.check_tiered_plugin === '1' ) {
+				$('.woo-custom-installments-group.variable-range-price').find('.woo-custom-installments-details-with-fee .best-value.fee-included .amount').html(last_fee_installment.price);
+				$('.woocommerce-variation-price').find('.woo-custom-installments-details-with-fee .best-value.fee-included .amount').html(last_fee_installment.price);
+
+				if ( $('#woo-custom-installments-product-price').hasClass('active') ) {
+					$('#woo-custom-installments-product-price').find('.woo-custom-installments-details-with-fee .best-value.fee-included .amount').html(last_fee_installment.price);
+				}
+			}
+
+			/**
+			 * Update price elements on popup and accordion on change variation
+			 *
+			 * @since 4.5.0
+			 * @version 5.1.0
+			 * @package MeuMouse.com
+			 */
+			$.ajax({
+				url: wci_update_table_params.ajax_url,
+				type: 'POST',
+				dataType: 'json',
+				data: {
+					action: 'get_updated_variation_prices_action',
+					variation_id: get_variation_id,
+					direct_price: direct_price,
+				},
+				success: function(response) {
+					if (response.success) {
+						$.each(response.data, function(key, value) {
+							$.each(value.selectors, function(index, selector) {
+								$(selector).find('.amount').html(value.price);
+							});
+						});
+					}
+				},
+				error: function(error) {
+					console.log(error);
+				},
+			});
+	},
   
 	  /**
 	   * Formatted price
 	   *
+	   * @since 2.3.5
 	   * @param {string} price
+	   * @returns array
 	   */
 	  get_formatted_price: function(price) {
 		'use strict';
@@ -101,7 +177,7 @@ jQuery(document).ready( function($) {
 		  decimal: wci_update_table_params.currency_format_decimal_sep,
 		  thousand: wci_update_table_params.currency_format_thousand_sep,
 		  precision: wci_update_table_params.currency_format_num_decimals,
-		  format: wci_update_table_params.currency_format
+		  format: wci_update_table_params.currency_format,
 		} );
   
 		return formatted_price;
